c41d00223edb79152f7801c45ea7e970de796651
imsejin <hackeres123@gmail.com>
2025-07-17 14:32:11 +0900

    feat: 사용자 엔터티 추가

--- modules/jpa/src/main/java/com/loopers/domain/BaseEntity.java
+++ modules/jpa/src/main/java/com/loopers/domain/BaseEntity.java
@@ -1,13 +1,11 @@
 package com.loopers.domain;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import lombok.Getter;
+
 import java.time.ZonedDateTime;
 
 /**
@@ -17,10 +15,6 @@ import java.time.ZonedDateTime;
 @MappedSuperclass
 @Getter
 public abstract class BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private final Long id = 0L;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private ZonedDateTime createdAt;
