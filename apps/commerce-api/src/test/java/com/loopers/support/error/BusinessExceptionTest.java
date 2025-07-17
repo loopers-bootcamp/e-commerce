ce4f8f8e90cf0f4e19a7d4698f5b61cfcdcc1f99
imsejin <hackeres123@gmail.com>
2025-07-17 14:28:22 +0900

    refactor: 공통 예외, 에러타입 리팩터링

--- apps/commerce-api/src/test/java/com/loopers/support/error/BusinessExceptionTest.java
+++ apps/commerce-api/src/test/java/com/loopers/support/error/BusinessExceptionTest.java
@@ -5,16 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class CoreExceptionTest {
+class BusinessExceptionTest {
+
     @DisplayName("ErrorType 기반의 예외 생성 시, 별도의 메시지가 주어지지 않으면 ErrorType의 메시지를 사용한다.")
     @Test
     void messageShouldBeErrorTypeMessage_whenCustomMessageIsNull() {
         // arrange
-        ErrorType[] errorTypes = ErrorType.values();
+        CommonErrorType[] errorTypes = CommonErrorType.values();
 
         // act & assert
-        for (ErrorType errorType : errorTypes) {
-            CoreException exception = new CoreException(errorType);
+        for (CommonErrorType errorType : errorTypes) {
+            BusinessException exception = new BusinessException(errorType);
             assertThat(exception.getMessage()).isEqualTo(errorType.getMessage());
         }
     }
@@ -26,9 +27,10 @@ class CoreExceptionTest {
         String customMessage = "custom message";
 
         // act
-        CoreException exception = new CoreException(ErrorType.INTERNAL_ERROR, customMessage);
+        BusinessException exception = new BusinessException(CommonErrorType.INTERNAL_ERROR, customMessage);
 
         // assert
         assertThat(exception.getMessage()).isEqualTo(customMessage);
     }
+
 }
