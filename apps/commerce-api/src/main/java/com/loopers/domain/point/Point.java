cdbbb78851e8b2df4e86f4d52d321a2923acdbf4
imsejin <hackeres123@gmail.com>
2025-07-17 15:41:06 +0900

    feat: 포인트 엔터티 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/Point.java
@@ -0,0 +1,92 @@
+package com.loopers.domain.point;
+
+import com.loopers.domain.BaseEntity;
+import com.loopers.domain.point.error.PointErrorType;
+import com.loopers.support.error.BusinessException;
+import com.loopers.support.error.CommonErrorType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "point")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Point extends BaseEntity {
+
+    /**
+     * 아이디
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "point_id", nullable = false, updatable = false)
+    private Long id;
+
+    /**
+     * 잔액
+     */
+    @Column(name = "balance", nullable = false)
+    private Long balance;
+
+    // -------------------------------------------------------------------------------------------------
+
+    /**
+     * 사용자 아이디
+     */
+    @Column(name = "ref_user_id", nullable = false)
+    private Long userId;
+
+    // -------------------------------------------------------------------------------------------------
+
+    private static final long MAX_BALANCE = 100_000_000L;
+
+    @Builder
+    private Point(Long balance, Long userId) {
+        if (balance == null || balance < 0) {
+            throw new BusinessException(CommonErrorType.INVALID,
+                    "포인트는 0 이상의 값이어야 합니다.");
+        }
+
+        if (balance > MAX_BALANCE) {
+            throw new BusinessException(PointErrorType.TOO_MUCH_BALANCE);
+        }
+
+        if (userId == null) {
+            throw new BusinessException(CommonErrorType.INVALID);
+        }
+
+        this.balance = balance;
+        this.userId = userId;
+    }
+
+    public void increase(long amount) {
+        if (amount <= 0) {
+            throw new BusinessException(CommonErrorType.INVALID,
+                    "0 이하의 값으로 포인트를 증가할 수 없습니다.");
+        }
+
+        long increaseBalance = this.balance + amount;
+        if (increaseBalance > MAX_BALANCE) {
+            throw new BusinessException(PointErrorType.TOO_MUCH_BALANCE);
+        }
+
+        this.balance = increaseBalance;
+    }
+
+    public void decrease(long amount) {
+        if (amount <= 0) {
+            throw new BusinessException(CommonErrorType.INVALID,
+                    "0 이하의 값으로 포인트를 감소할 수 없습니다.");
+        }
+
+        long decreasedBalance = this.balance - amount;
+        if (decreasedBalance < 0) {
+            throw new BusinessException(PointErrorType.NOT_ENOUGH_BALANCE);
+        }
+
+        this.balance = decreasedBalance;
+    }
+
+}
