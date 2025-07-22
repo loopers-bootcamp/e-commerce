38ceeaf35a5e73bfd45b82377da93573343b8845
imsejin <hackeres123@gmail.com>
2025-07-22 17:55:57 +0900

    test: 성별 VO 직렬화 테스트 추가

--- apps/commerce-api/src/test/java/com/loopers/domain/user/attribute/GenderTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/user/attribute/GenderTest.java
@@ -1,5 +1,7 @@
 package com.loopers.domain.user.attribute;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -34,6 +36,35 @@ class GenderTest {
 
         // then
         assertThat(gender).isNull();
+    }
+
+    @DisplayName("직렬화하면 단순 숫자가 된다.")
+    @EnumSource(Gender.class)
+    @ParameterizedTest
+    void convertToNumber_whenJacksonSerialize(Gender gender) throws JsonProcessingException {
+        // given
+        JsonMapper mapper = new JsonMapper();
+
+        // when
+        String json = mapper.writeValueAsString(gender);
+
+        // then
+        assertThat(json).isEqualTo("%d".formatted(gender.getCode()));
+    }
+
+    @DisplayName("숫자를 사용하여 역직렬화된다.")
+    @EnumSource(Gender.class)
+    @ParameterizedTest
+    void convertNumber_whenJacksonDeserialize(Gender gender) throws JsonProcessingException {
+        // given
+        JsonMapper mapper = new JsonMapper();
+        String json = "%d".formatted(gender.getCode());
+
+        // when
+        Gender convertedGender = mapper.readValue(json, Gender.class);
+
+        // then
+        assertThat(convertedGender).isEqualTo(gender);
     }
 
 }
