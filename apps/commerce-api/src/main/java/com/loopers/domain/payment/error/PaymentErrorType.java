e2a7d33300c27f943aadaea2992105f00c79259e
imsejin <hackeres123@gmail.com>
2025-07-31 18:37:56 +0900

    feat: 결제 애플리케이션 레이어 구현

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/error/PaymentErrorType.java
@@ -0,0 +1,26 @@
+package com.loopers.domain.payment.error;
+
+import com.loopers.support.error.ErrorType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Locale;
+
+@Getter
+@RequiredArgsConstructor
+public enum PaymentErrorType implements ErrorType {
+
+    UNPROCESSABLE(HttpStatus.UNPROCESSABLE_ENTITY, "결제할 수 없는 주문 건입니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    PaymentErrorType(HttpStatus status, String message) {
+        this.status = status;
+        this.code = "payment:" + name().toLowerCase(Locale.ROOT);
+        this.message = message;
+    }
+
+}
