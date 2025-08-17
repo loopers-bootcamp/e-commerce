b53b1fdc7f5e9fbd650e9f118b7c31517f835e52
imsejin <hackeres123@gmail.com>
2025-08-17 19:32:36 +0900

    feat: pg-simulator 모듈 추가

---
+++ apps/pg-simulator/src/main/kotlin/com/loopers/PaymentGatewayApplication.kt
@@ -0,0 +1,25 @@
+package com.loopers
+
+import jakarta.annotation.PostConstruct
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
+import java.util.*
+
+@EnableAsync
+@SpringBootApplication
+@ConfigurationPropertiesScan
+class PaymentGatewayApplication {
+
+    @PostConstruct
+    fun started() {
+        // set timezone
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<PaymentGatewayApplication>(*args)
+}
