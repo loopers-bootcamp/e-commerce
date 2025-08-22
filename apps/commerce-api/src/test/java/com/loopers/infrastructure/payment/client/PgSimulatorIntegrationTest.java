c7314e89e836b56fc58e96c26ef1aef2169b6036
imsejin <hackeres123@gmail.com>
2025-08-22 19:04:51 +0900

    test: 서킷브레이커 통합 테스트 추가

--- apps/commerce-api/src/test/java/com/loopers/infrastructure/payment/client/PgSimulatorIntegrationTest.java
+++ apps/commerce-api/src/test/java/com/loopers/infrastructure/payment/client/PgSimulatorIntegrationTest.java
@@ -17,14 +17,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.web.client.HttpServerErrorException;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
 import static com.loopers.resilience4j.test.CircuitBreakerAssertion.assertThatCircuitBreaker;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.instancio.Select.root;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @Slf4j
-class PgSimulatorTest {
+class PgSimulatorIntegrationTest {
 
     private static final String TRANSACT = "payment-gateway--transact";
     private static final String GET_TRANSACTIONS = "payment-gateway--get-transactions";
@@ -98,6 +104,60 @@ class PgSimulatorTest {
                     .hasNoSuccessCall();
 
             verify(client, times(minimumNumberOfCalls)).transact(anyString(), any());
+        }
+
+        @DisplayName("서킷브레이커가 열리는 것부터 다시 닫힐 때까지의 과정을 검증한다.")
+        @Test
+        void validateCircuitBreakerFromOpeningToClosingAgain() throws InterruptedException {
+            // given
+            PaymentGateway.Request.Transact request = mock(PaymentGateway.Request.Transact.class);
+            given(request.cardNumber()).willReturn(new CardNumber("0000000000000000"));
+            given(client.transact(anyString(), any())).willThrow(HttpServerErrorException.class);
+
+            CircuitBreaker circuitBreaker = registry.circuitBreaker(TRANSACT);
+            CircuitBreakerConfig config = circuitBreaker.getCircuitBreakerConfig();
+            int minimumNumberOfCalls = config.getMinimumNumberOfCalls();
+
+            List<CircuitBreaker.StateTransition> transitions = new CopyOnWriteArrayList<>();
+            circuitBreaker.getEventPublisher().onStateTransition(e -> transitions.add(e.getStateTransition()));
+
+            Integer randomCallCount = Instancio.of(Integer.class)
+                    .generate(root(), gen -> gen.ints().range(minimumNumberOfCalls, config.getSlidingWindowSize() - 1))
+                    .create();
+
+            // when & then
+            assertThatCircuitBreaker()
+                    .withCircuitBreaker(circuitBreaker)
+                    .withInitialState(CircuitBreaker.State.CLOSED)
+                    .withCallCount(minimumNumberOfCalls)
+                    .isExecutedBy(() -> sut.transact(request))
+                    .isDone()
+                    .isOpen()
+                    .hasNoSuccessCall();
+
+            verify(client, times(minimumNumberOfCalls)).transact(anyString(), any());
+
+            // given
+            reset(client);
+            given(client.transact(anyString(), any())).willAnswer(invocation -> {
+                PgSimulatorResponse.Transact data = new PgSimulatorResponse.Transact("", "SUCCESS", null);
+                return PgApiResponse.success(data);
+            });
+
+            // when
+            TimeUnit.SECONDS.sleep(1); // wait-duration-in-open-state
+            IntStream.range(0, config.getPermittedNumberOfCallsInHalfOpenState())
+                    .forEach(i -> sut.transact(request));
+            IntStream.range(0, randomCallCount + 1)
+                    .forEach(i -> sut.transact(request));
+
+            // then
+            assertThat(circuitBreaker.getState()).isEqualTo(CircuitBreaker.State.CLOSED);
+            assertThat(transitions).containsExactly(
+                    CircuitBreaker.StateTransition.CLOSED_TO_OPEN,
+                    CircuitBreaker.StateTransition.OPEN_TO_HALF_OPEN,
+                    CircuitBreaker.StateTransition.HALF_OPEN_TO_CLOSED
+            );
         }
 
     }
