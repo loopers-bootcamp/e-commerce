dae9c86e00f48ff9f491227d2d78101838dc1bb2
imsejin <hackeres123@gmail.com>
2025-09-01 21:44:31 +0900

    conf: kafka 모듈 사용

--- apps/commerce-api/build.gradle.kts
+++ apps/commerce-api/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     // add-ons
     implementation(project(":modules:jpa"))
     implementation(project(":modules:redis"))
+    implementation(project(":modules:kafka"))
     implementation(project(":modules:feign"))
     implementation(project(":modules:resilience4j"))
     implementation(project(":supports:jackson"))
