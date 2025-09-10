fbbdae5c2a5e8bbe3b14dfedf9911a4547ac7f32
imsejin <hackeres123@gmail.com>
2025-09-11 04:03:13 +0900

    feat: jackson 모듈에 WrapperJsonMapper 빈 추가

--- supports/jackson/src/main/java/com/loopers/config/jackson/JacksonConfig.java
+++ supports/jackson/src/main/java/com/loopers/config/jackson/JacksonConfig.java
@@ -3,7 +3,9 @@ package com.loopers.config.jackson;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -45,4 +47,11 @@ class JacksonConfig {
             );
         };
     }
+
+    @Bean
+    public WrappedJsonMapper wrappedJsonMapper(ObjectMapper objectMapper) {
+        JsonMapper jsonMapper = JsonMapper.builder(objectMapper.getFactory()).build();
+        return new WrappedJsonMapper(jsonMapper);
+    }
+
 }
