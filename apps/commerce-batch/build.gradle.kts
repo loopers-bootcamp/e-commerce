430ca218d359b1d517035a15c72bb9492dfd44a7
imsejin <hackeres123@gmail.com>
2025-09-16 00:58:05 +0900

    feat: batch app 추가

---
+++ apps/commerce-batch/build.gradle.kts
@@ -0,0 +1,19 @@
+dependencies {
+    // add-ons
+    implementation(project(":modules:jpa"))
+    implementation(project(":modules:redis"))
+    implementation(project(":supports:jackson"))
+    implementation(project(":supports:logging"))
+
+    // batch
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+
+    // querydsl
+    annotationProcessor("com.querydsl:querydsl-apt::jakarta")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+
+    // test-fixtures
+    testImplementation(testFixtures(project(":modules:jpa")))
+    testImplementation(testFixtures(project(":modules:redis")))
+}
